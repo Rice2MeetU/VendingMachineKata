5c367d580105df49e34173550acf306e5495a3e9
Alex <asaenbandith@gmail.com>
2015-09-04 17:22:06 -0500

    Initial commit to add starting project files to repository

---
+++ src/vendingMachine/main.java
@@ -0,0 +1,10 @@
+package vendingMachine;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the world's best vending machine.....probably");
+
+	}
+
+}
