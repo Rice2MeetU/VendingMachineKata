ed3f54b52ce16af9f12e7ea59062815fcc9c317d
Alex <asaenbandith@gmail.com>
2015-09-04 19:30:04 -0500

    Coin recognition prototype established. Currently loops infinitely

--- src/vendingMachine/VendingBrain.java
+++ src/vendingMachine/VendingBrain.java
@@ -2,13 +2,55 @@ package vendingMachine;
 
 public class VendingBrain {
 	
+	private double insertedTotal = 0.00;
+	private double availableChange = 10.00;
+	private double availableCoinReturn = 0.00;
+	
+	
+	// Coin reading method. Verifies whether an insert is valid and then acts based on this ruling.
 	public void readCoin(String coinInput){
 		if(coinInput.equals("q") || coinInput.equals("Q")){
 			System.out.println("Quarter inserted");
+			updateInsertedTotal(0.25);
+			
 		}
+		
+		else if(coinInput.equals("n") || coinInput.equals("N")){
+			System.out.println("Nickel inserted");
+			updateInsertedTotal(0.05);
+		}
+		
+		else if(coinInput.equals("d") || coinInput.equals("D")){
+			System.out.println("Dime inserted");
+			updateInsertedTotal(0.10);
+		}
+		
+		else if(coinInput.equals("p") || coinInput.equals("P")){
+			System.out.println("Sorry, pennies are not accepted because reasons");
+			updateCoinReturnTotal(0.01);
+		}
+		
 		else {
-			System.out.println("Unsuccesful insertion");
+			System.out.println("Error, unrecognizable coin");
 			System.out.println("Coin input was: " + coinInput);
 		}
 	}
+	
+	public void showDisplay(){
+		if (insertedTotal == 0.00){
+			System.out.println("Display: Insert Coin");
+		}
+		else {
+			System.out.println("Display: " + insertedTotal);
+		}
+	}
+	
+	public void updateInsertedTotal(Double coinValue){
+		insertedTotal = insertedTotal + coinValue;
+	}
+	
+	public void updateCoinReturnTotal(Double returnCoins){
+		
+	}
+	
 }
--- src/vendingMachine/main.java
+++ src/vendingMachine/main.java
@@ -6,10 +6,18 @@ public class main {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		VendingBrain firstBrain = new VendingBrain();
+		boolean didYouLeave = false;
 		
-		System.out.println("Welcome to the world's best vending machine.....probably");
-		System.out.println("Insert Coin");
-		firstBrain.readCoin(keyboard.nextLine());
+		System.out.println("Welcome to the world's best vending machine..........probably.");
+		//System.out.println("Please choose an option below");
+		
+		//Currently will loop infinitely but that's fine for now
+		while (didYouLeave == false){
+			firstBrain.showDisplay();
+			firstBrain.readCoin(keyboard.nextLine());
+		}
+		
+		
 
 	}
 
